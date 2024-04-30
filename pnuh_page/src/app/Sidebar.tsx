8ddf775d0d7b064e0d02a879919693d67a553e17
kkami91 <ghkdth919@gamil.com>
2024-04-30 18:45:14 +0900

    first

---
+++ pnuh_page/src/app/Sidebar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen w-64 bg-gray-800 text-white p-4">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/test">Test</Link>
+          </li>
+          <li>
+            <Link href="/raw">Raw Data</Link>
+          </li>
+          <li>
+            <Link href="/stress">Stress Score</Link>
+          </li>
+          {/* 추가적인 메뉴 항목을 여기에 추가할 수 있습니다. */}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ pnuh_page/src/app/stress/page.tsx
@@ -0,0 +1,13 @@
+const TestPage = () => {
+    return (
+      <div className="p-8">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Stress Page</h1>
+        <p className="text-lg mb-8">This is a sample page created using Next.js, TypeScript, and Tailwind CSS.</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click me
+        </button>
+      </div>
+    );
+  };
+  
+  export default TestPage;
